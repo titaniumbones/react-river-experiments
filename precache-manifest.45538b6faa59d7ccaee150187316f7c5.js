699db7c0649d43e55ae8735281934fcea221121d
Matt Price <matt.price@utoronto.ca>
2019-12-23 16:48:53 -0500

    Updates

---
+++ precache-manifest.45538b6faa59d7ccaee150187316f7c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "464eb34a5deb09aa5076ca524745a34b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0888f47b11082ddb2de4",
+    "url": "/static/css/2.75d0805f.chunk.css"
+  },
+  {
+    "revision": "7982644b9bff00ee0c8a",
+    "url": "/static/css/main.6d816f45.chunk.css"
+  },
+  {
+    "revision": "0888f47b11082ddb2de4",
+    "url": "/static/js/2.ea2b455a.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/static/js/2.ea2b455a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7982644b9bff00ee0c8a",
+    "url": "/static/js/main.15093d28.chunk.js"
+  },
+  {
+    "revision": "e3774892864f7e6356c8",
+    "url": "/static/js/runtime-main.1b31c423.js"
+  }
+]);
