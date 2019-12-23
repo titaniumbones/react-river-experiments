d301b70384b9505567198451f3215673921ecda0
Matt Price <matt.price@utoronto.ca>
2019-12-23 10:42:11 -0500

    rivertabs: extract to own component, stop storing state
    - state now belongs to waterchart & lives in redux store
    - simplify invocation of waterchart
    - Points & PointsList live in RiverTabs.js, too.

---
+++ src/components/riverTabPanel.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import MarkdownFromUrl from './markdownFromUrl.js'
+import Waterchart from '../components/waterchart.js'
+
+export class Point extends React.Component {
+  render() {
+    const l=this.props.location,
+          url=`https://www.google.com/maps/dir/?api=1&destination=${l[0]},${l[1]}`
+    return (
+      <li>
+        <a href={url}>{this.props.name}</a>
+      </li>
+    )
+  }
+}
+
+export class PointList extends React.Component {
+  render() {
+    const pEntries = Object.entries(this.props.points)
+    return (
+      <div>
+      <h2 value="Map Links">Map Links</h2>
+      <ul>
+      {pEntries.length > 0 ?
+       pEntries.map(point => {
+          return <Point name={point[0]} location={point[1]}/>
+       }) :
+       <li>No Point Locations Provided</li>
+      } 
+      </ul>
+      </div>
+    )
+  }
+}
+
+export default class RiverTabPanel extends React.Component {
+  render() {
+    const {points, slug} = this.props.river
+    return (
+      <div>
+        <Waterchart spotslug={slug}
+        />
+        {points && <PointList points={points}/>}
+        <MarkdownFromUrl url={`./descriptions/${slug}.md`}/>
+        </div>
+    )
+  }
+}
+
+
--- src/components/watertabs.js
+++ src/components/watertabs.js
@@ -3,84 +3,8 @@ import MarkdownFromUrl from './markdownFromUrl.js'
 import {processGauge} from '../DataParsers.js'
 import Waterchart from '../components/waterchart.js'
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
-
-
-
-export class Point extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      location: this.props.location
-    }
-  }
-  render() {
-    const l=this.props.location,
-          url=`https://www.google.com/maps/dir/?api=1&destination=${l[0]},${l[1]}`
-    return (
-      <li>
-        <a href={url}>{this.props.name}</a>
-      </li>
-    )
-  }
-}
-
-
-
-
-export class PointList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-
-    }
-  } 
-  render() {
-    const pEntries = Object.entries(this.props.points)
-    return (
-      <div>
-      <h2 value="Map Links">Map Links</h2>
-      <ul>
-      {pEntries.length > 0 ?
-       pEntries.map(point => {
-          return <Point name={point[0]} location={point[1]}/>
-       }) :
-       <li>No Point Locations Provided</li>
-      } 
-      </ul>
-      </div>
-    )
-  }
-}
-
-export class RiverTabPanel extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      points: this.props.river.points,
-      slug: this.props.river.slug,
-      url: `./descriptions/${this.props.slug}.md`,
-      latestData: null
-    }
-    processGauge(this.props.river)
-      .then( data => this.setState({latestData: data}))
-  }
-  componentDidMount = () => {
- 
-  }
-  
-  render() {
-    const url=`./descriptions/${this.state.slug}.md`
-    return (
-      <div>
-        <Waterchart seriesdata={this.state.latestData}/>
-        {this.props.river.points && <PointList points={this.props.river.points}/>}
-        <MarkdownFromUrl url={`./descriptions/${this.state.slug}.md`}/>
-        </div>
-    )
-  }
-}
-
+import 'react-tabs/style/react-tabs.css'
+import RiverTabPanel from './riverTabPanel.js'
 export default class WaterTabs extends Component {
   constructor(props) {
     super(props)
