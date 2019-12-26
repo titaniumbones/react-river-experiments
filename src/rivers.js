4a51a8d166916086e8b4228d3b63d5754aedf7c5
Matt Price <matt.price@utoronto.ca>
2019-12-25 20:47:18 -0500

    rivers.js: add sources fields (prepare for updat of chart gen code)

--- src/rivers.js
+++ src/rivers.js
@@ -7,23 +7,8 @@ const streetsville = {
   "lat":123.456,
   "long": -456.789,
   "levels": [[0,4.6,"bad"], [4.6,8,"shoulder"], [8,50,"good"],[50,100,"shoulder"]],
-  "qualityPeaks": [ [0,"bad"], [85, "good"], [170, "bad"], [290, "bad"] ],
   "minHeight": 4.6
 },
-      // irvine =   {
-      //   "name": "Irvine",
-      //   "gaugeType": "wateroffice",
-      //   "gaugeID": "02GA005",
-      //   "slug": "irvine",
-      //   "units": "cms",
-      //   "points":{ 
-      //     "putin": [43.702321,-80.445578] ,
-      //     "takeout": [43.662701, -80.453265] 
-      //   },
-      //   "levels": [[0,4.6,"bad"], [4.6,8,"shoulder"], [8,50,"good"],[50,100,"shoulder"]],
-      //   "qualityPeaks": [ [0,"bad"], [85, "good"], [170, "bad"], [290, "bad"] ],
-      //   "minHeight": 4.6
-      // },
       irvine =   {
         "name": "Irvine",
         "gaugeType": "grca",
@@ -36,8 +21,19 @@ const streetsville = {
           "takeout": [43.662701, -80.453265] 
         },
         "levels": [[0,4.6,"bad"], [4.6,8,"shoulder"], [8,50,"good"],[50,100,"shoulder"]],
-        "qualityPeaks": [ [0,"bad"], [85, "good"], [170, "bad"], [290, "bad"] ],
-        "minHeight": 4.6
+        "minHeight": 4.6,
+        sources: [
+          {type: 'grca',
+           timeseries: [8773042],
+           units: 'cms',
+           // TODO: get stationID
+           // id: xxx
+          },
+          {type: 'wateroffice',
+           timeseries: 47,
+           units: 'cms',
+           id: '02GA005'}
+        ]
       },
 
       elora = {
@@ -55,6 +51,12 @@ const streetsville = {
         },
         minHeight: 5,
         levels: [[0,4.6,"bad"], [4.6,8,"shoulder"], [8,50,"good"],[50,100,"shoulder"]],
+        sources: [{
+          type: 'grca',
+          timeseries: 7405042,
+          units: 'cms',
+          
+        }]
       },
       upperCredit = {
         name: "Upper Credit River at Belfountain",
@@ -62,15 +64,22 @@ const streetsville = {
         gaugeID: '14522010',
         "units": "cms",
         slug: "uppercredit",
-        points: {},
+        points: {
+          putin: [43.7962886,-80.0139645,15],
+          takeout: [43.802320,-79.993910]
+        },
         minHeight: 0.75,
         levels: [[0,0.75,"bad"], [0.75, 0.78,"shoulder"], [0.78, 1.1,"good"],[1.1,100,"shoulder"]],
-      },
-      rivers =[elora, streetsville, upperCredit, irvine];
+        sources: [{
+          type: 'cvc',
+          timeseries: 1452210,
+          units: 'cms',
+          id: null
+        }]
+      }
 
 
-const cors = `https://hackinghistory.ca:9090/`, // `https://cors-anywhere.herokuapp.com/`
-      Rivers =[elora, streetsville, upperCredit, irvine];
+const Rivers =[elora, streetsville, upperCredit, irvine];
 
 export {streetsville as lowercredit}
 export default Rivers;
