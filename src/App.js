49ecb59a12321f2c31a08315da750d3075030c66
Matt Price <matt.price@utoronto.ca>
2020-01-05 12:41:22 -0500

    move most action from `index` to `App`
    Enough is  now happening in `App` component that it seems wise to
    export it as its own component for integration in endex.  Esp b/c it
    now needs to be wrapped in a firebase HOC (which I still ldon't quite
    understand, obvs).

--- src/App.js
+++ src/App.js
@@ -1,26 +1,101 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Rivers from  './rivers.js';
+import WaterTabs from './components/watertabs.js'
+import WaveTabs from './components/WaveTabs.js'
+import MarkdownFromUrl from './components/markdownFromUrl.js'
+import Journal from './components/journal.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import {
+  Router,
+  Link
+} from "@reach/router";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import mainReducer from './reducers/mainReducer.js'
+import { combineReducers } from 'redux'
+import { persistStore, persistReducer } from 'redux-persist'
+// TODO: switch from localStorage to indexedDB
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+// this wass helpful re: thunk + devtools:
+// https://medium.com/@e_himmelfarb/implement-redux-devtools-extension-with-thunk-and-other-async-middleware-20e97100b2b0
+import thunk from 'redux-thunk'; // no changes here 😀
+import firebase, {dbRef, providers} from './firebase'
+import 'firebase/auth';
+import withFirebaseAuth, { WrappedComponentProps } from 'react-with-firebase-auth';
+const persistConfig = {
+  key: 'root',
+  storage,
 }
 
-export default App;
+
+
+const persistedReducer = persistReducer(persistConfig, mainReducer)
+
+const store = createStore(persistedReducer,
+                          compose (applyMiddleware(thunk), 
+                                   window.__REDUX_DEVTOOLS_EXTENSION__ ?
+                                   window.__REDUX_DEVTOOLS_EXTENSION__() : f => f));
+const persistor = persistStore(store)
+
+// const Chartist = require('chartist')
+
+
+
+
+function App ({
+  /** These props are provided by withFirebaseAuth HOC */
+  signInWithEmailAndPassword,
+  createUserWithEmailAndPassword,
+  signInWithGoogle,
+  signInWithFacebook,
+  signInWithGithub,
+  signInWithTwitter,
+  signInAnonymously,
+  signOut,
+  setError,
+  user,
+  error,
+  loading,
+}: WrappedComponentProps) {
+  //console.log(signInWithGoogle)
+  return (
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+
+        <nav className="nav bg-light">
+          <Link className="brand" to="">S.O. Shreds</Link>
+          <Link to="rivers">Rivers: Current</Link>
+      <Link to="waves">Waves: Current</Link>
+          <Link to="journal">Journal</Link>
+          <Link to="about">About</Link>
+          <div className="nav-right">
+          {
+            user
+              ? <a href="#">Hello, {user.displayName}</a>
+            : <button onClick={signInWithGoogle}>Sign in with Google</button>
+          }
+            {
+              user && 
+                <button class="bg-dark text-light" onClick={signOut}>Sign out</button>
+            }
+          </div>
+        </nav>
+
+        
+        <Router>
+          <WaterTabs path="rivers/*" rivers={Rivers}/>
+          <WaveTabs path="waves/*" />
+          <Journal path="journal" rivers={Rivers}/>
+          <MarkdownFromUrl path="about" url="descriptions/about.md"/>
+        </Router>
+        
+      </PersistGate>
+    </Provider>
+  )
+}
+
+export default withFirebaseAuth({
+  providers: providers,
+  firebaseAppAuth: firebase.auth(),
+})(App)
--- src/index.js
+++ src/index.js
@@ -1,79 +1,36 @@
 import '../node_modules/chartist/dist/chartist.min.css'
-import React from 'react';
-import ReactDOM from 'react-dom';
-// import '../node_modules/chota/dist/chota.css'
+import '../node_modules/chartist-plugin-tooltips-updated/dist/chartist-plugin-tooltip.css'
+import './chota.css'
 import './index.css';
 import './rivers.css';
-import Rivers from  './rivers.js';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
-import WaterTabs from './components/watertabs.js'
-import WaveTabs from './components/WaveTabs.js'
-import MarkdownFromUrl from './components/markdownFromUrl.js'
-import Journal from './components/journal.js'
-import 'react-tabs/style/react-tabs.css';
-import {
-  Router,
-  Link
-} from "@reach/router";
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, compose } from 'redux';
-import mainReducer from './reducers/mainReducer.js'
-import { combineReducers } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
+// import {
+//   Router,
+//   Link
+// } from "@reach/router";
+// import { Provider } from 'react-redux';
+// import { createStore, applyMiddleware, compose } from 'redux';
+// import mainReducer from './reducers/mainReducer.js'
+// import { combineReducers } from 'redux'
+// import { persistStore, persistReducer } from 'redux-persist'
 // TODO: switch from localStorage to indexedDB
-import { PersistGate } from 'redux-persist/integration/react'
-import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
-// import firebase from 'firebase/app'
-// import 'firebase/auth'
+// import { PersistGate } from 'redux-persist/integration/react'
+// import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
+import firebase from 'firebase/app'
+import 'firebase/auth'
 // import { ReactReduxFirebaseProvider, firebaseReducer } from 'react-redux-firebase'
-import thunk from 'redux-thunk'; // no changes here 😀
-
-
-// // Add firebase to reducers
-// const rootReducer = combineReducers({
-//   firebase: firebaseReducer
-//   // firestore: firestoreReducer // <- needed if using firestore
-// })
-
-const persistConfig = {
-  key: 'root',
-  storage,
-}
+// this wass helpful re: thunk + devtools:
+// https://medium.com/@e_himmelfarb/implement-redux-devtools-extension-with-thunk-and-other-async-middleware-20e97100b2b0
+// import thunk from 'redux-thunk'; // no changes here 😀
+// import firebase, {dbRef, providers} from './firebase'
+// import 'firebase/auth';
+// import withFirebaseAuth, { WrappedComponentProps } from 'react-with-firebase-auth';
+import App from './App.js'
 
 
 
-const persistedReducer = persistReducer(persistConfig, mainReducer)
-
-const store = createStore(persistedReducer,
-                          compose (applyMiddleware(thunk), 
-                                   window.__REDUX_DEVTOOLS_EXTENSION__ ?
-                                   window.__REDUX_DEVTOOLS_EXTENSION__() : f => f));
-const persistor = persistStore(store)
-
-// const Chartist = require('chartist')
-
-function App(props) {
-  return (
-    <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-        <nav className="nav bg-light">
-          <Link className="brand" to="">S.O. Shreds</Link>
-          <Link to="rivers">Rivers: Current Levels</Link>
-          <Link to="waves">Waves_Current Conditions</Link>
-          <Link to="journal">Journal</Link>
-          <Link to="about">About</Link>
-        </nav>
-
-        <Router>
-          <WaterTabs path="rivers" rivers={Rivers}/>
-          <WaveTabs path="waves" />
-          <Journal path="journal" rivers={Rivers}/>
-          <MarkdownFromUrl path="about" url="descriptions/about.md"/>
-    </Router>
-    </PersistGate>
-    </Provider>
-  )
-}
 
 ReactDOM.render(
   <App />,
