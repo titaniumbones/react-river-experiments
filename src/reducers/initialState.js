69f567a993dac9883df9c730af20029c9d2ac4bb
Matt Price <matt.price@utoronto.ca>
2020-01-06 14:12:17 -0500

    initial state: make available to others
    needed access in order to reset filters & keep code DRY

---
+++ src/reducers/initialState.js
@@ -0,0 +1,10 @@
+export const initialState = {
+  journal: {
+    anonymous: []
+  },
+  journalFilter: {rating: false, last: false, first: 0, spot: false},
+  charts: [],
+  current: [],
+  ui: {},
+  user: null
+}
