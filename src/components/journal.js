08bfc9d2d50acc333f50526da7bf34d8698f311c
Matt Price <matt.price@utoronto.ca>
2020-01-06 14:22:17 -0500

    journal: add filter, do some reorganizing
    things got a bit ocmplicated here, hope I remember all I did!

--- src/components/journal.js
+++ src/components/journal.js
@@ -1,56 +1,83 @@
 import '../../node_modules/chartist/dist/chartist.min.css'
 import React from 'react';
 import '../index.css';
-import { connect } from 'react-redux';
-import SessionInfo from '../components/sessionInfo.js'
+import moment from 'moment'
+import { connect, useDispatch } from 'react-redux';
 import JournalForm from './journalForm.js'
 import JournalRow from './journalRow.js'
+import JournalFilter from './journalFilter.js'
+import  {journalRef,} from '../firebase'
+import {compareJournals} from '../utils/utils.js'
+import {mapJournalToProps as mapStateToProps} from '../utils/stateMaps.js'
+import store from '../store.js'
+import { filterEntries} from '../utils/filters.jsx'
 
-// 'state' is actually *redux store*,
-// which is I guess a 'state', but obvs *not*
-// plain old react state.  
-function mapStateToProps(state) {
-  return {
-    entries: state.journal.entries
-  };
-}
 
 export class Journal extends React.Component {
+  
+  constructor(props) {
+    super(props)
+    this.props.uid && journalRef
+      .child(this.props.uid)
+      .on('value', (snapshot) => {
+        compareJournals(store.getState().journal, snapshot.val())
+      })
+  }
 
-   render() {
+  render() {
+   
     console.log("rendering main journal")
-    console.log(this.props.entries)
+    console.log('MAINJOURNALENTRIES', this.props.entries)
+    
     return (
       <div className="journal">
         <JournalForm submit="add"/>
-        {this.props.entries && <JournalAllEntries entries={this.props.entries}/>}
+        <JournalFilter/>
+        <JournalAllEntries
+          filters={this.props.filters}
+          uid={this.props.uid}
+          entries={this.props.entries}/>
       </div>
     )
   }
 }
 
-
 class JournalAllEntries extends React.Component {
-  
+
+  componentDidMount = () => {
+
+  }
   render() {
     console.log("rerendering entries")
     console.log('ALLENTRIES', this.props.entries)
-    return (
-      <div className="journal-entries">
-        {/* <JournalRow spot="MEMEM" date="DATEDATE" entry="whatevs"/> */}
-        {this.props.entries.map( (dataObj, index) =>
-          <JournalRow key={dataObj.id} id={dataObj.id} spot={dataObj.spot}
-                      date={dataObj.date} entry={dataObj.entry}
-                      showForm={false}/> )}
-        {this.props.entries.forEach( (entry, index) => console.log(entry, index))}
-      </div>
-    )
+    if (this.props.entries && this.props.entries.length > 0){
+      const visibleEntries = this.props.filters ?
+            filterEntries(this.props.filters, this.props.entries) :
+            this.props.entries
+      console.log('ALLENTRIESFILTERED', this.props.filters,visibleEntries)
+      return (
+      
+        <div className="journal-entries">
+          {/* <JournalRow spot="MEMEM" date="DATEDATE" entry="whatevs"/> */}
+          {visibleEntries.sort((a,b) => {
+            return (moment(a.date).valueOf() > moment(b.date).valueOf()) ?
+              -1 : 1
+          })
+           .map( (dataObj, index) =>
+             <JournalRow key={dataObj.id} id={dataObj.id} spot={dataObj.spot}
+                         date={dataObj.date} entry={dataObj.entry} rating={dataObj.rating}
+                         showForm={false}/> )}
+          {this.props.entries.forEach( (entry, index) => console.log('MAPPINGENTRIES',entry, index))}
+        </div>
+      
+      )
+    }
+    return null
   }
 }
 
 
-
-
+// const JournalAllEntries = connect(mapStateToProps)(JournalAllEntriesBase)
 
 
 export default connect(mapStateToProps)(Journal)
