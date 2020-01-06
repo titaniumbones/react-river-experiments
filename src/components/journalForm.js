615dc944a2f312a5dd281b1128d8152a8f65eff6
Matt Price <matt.price@utoronto.ca>
2020-01-06 14:19:19 -0500

    journalform: abstract 'selectspot' to own component. add rating to form

--- src/components/journalForm.js
+++ src/components/journalForm.js
@@ -4,43 +4,82 @@ import uuid from 'uuid/v4'
 import Rivers from  '../rivers.js';
 import Breaks from '../surfspots.js'
 
-import {addJournalEntry} from '../actions/main.js'
+// import {addJournalEntry} from '../actions/main.js'
 import {dbRef, journalRef, chartsRef}  from '../firebase.js'
 import firebase from '../firebase.js'
 import moment from 'moment'
-// 'state' is actually *redux store*,
-// which is I guess a 'state', but obvs *not*
-// plain old react state.  
-function mapStateToProps(state) {
-  return {
-    entries: state.journal.entries,
-    uid: state.user.user
-  };
+import {mapJournalToProps as mapStateToProps} from '../utils/stateMaps.js'
+import StarRating from './star-rating.jsx'
+
+
+export class SelectSpot extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      // spot: this.props.spot || (this.props.allowNull && 'none')
+    }
+  }
+  handleChange = (event) => {
+    const target = event.target;
+    const value = target.type ==='checkbox' ? target.checked : target.value;
+    const name = target.name;
+    
+    this.setState({[name]: value})    
+  }
+  render () {
+    return (
+      <label>
+        Spot:
+        <select name="spot" value={this.props.current || this.state.spot } onChange={this.props.onChange}>
+          {this.props.allowNull &&
+           <optgroup label="Clear"><option key="none" value="none">No spot</option></optgroup>}
+          <optgroup label="Rivers:">
+            {Rivers.map( (r, index) =>
+              <option key={index} value={r.slug}>{r.name}</option>
+            )}
+          </optgroup>
+          <optgroup label="Surf Breaks">
+            {Breaks.map( (r, index) =>
+              <option key={index} value={r.slug}>{r.name}</option>
+            )}
+
+          </optgroup>
+        </select>
+      </label>
+    )
+  }
 }
 
 export class JournalForm extends React.Component {
   constructor(props) {
     super(props)
-    const {spot, date, entry, id} = this.props
+    const {spot, date, entry, id, rating} = this.props
     this.state = {
       spot: spot || "grand",
       date: date || moment().format('YYYY-MM-DD'),
       entry: entry,
+      rating: rating,
       id : id || uuid()
     }
   }
   handleSubmit = (entry) => {
     const uid = this.props.uid || 'anonymous'
     console.log('FORMUPDATE', entry);
-    firebase.database().ref(`journal/${this.props.uid}/${entry.id}`).set({
-      spot: entry.spot,
-      date: entry.date,
-      entry: entry.entry
-    })
     this.props.dispatch({type: (this.props.submit === "update") ?
                          'UPDATE_ENTRY' : 'ADD_ENTRY',
                          payload: entry})
-  }
+      
+    this.props.uid && firebase.database()
+      .ref(`journal/${this.props.uid}/${entry.id}`)
+      .set({
+        spot: entry.spot,
+        date: entry.date,
+        entry: entry.entry,
+        rating: entry.rating,
+        updated: entry.updated
+      })
+  }  
 
   handleChange = (event) => {
     const target = event.target;
@@ -50,43 +89,43 @@ export class JournalForm extends React.Component {
   }
 
   onSubmit = (event) => {
-    const {spot, date,entry,id} = this.state;
+    const {spot, date,entry,id, rating} = this.state;
     const info = {
       spot: spot,
       date: date,
       entry: entry,
-      id: id 
+      id: id,
+      rating: rating,
+      updated: Date.now()
     };
     event.preventDefault();
     this.handleSubmit(info)
-    this.state.entry = this.state.date =  '';
+    this.state.entry =   '';
   }
+
+  setRating = rating => {
+    console.log('SETRATING', typeof (rating))
+    rating= Number(rating)
+    this.setState({ rating: rating });
+  };
 
   render() {
     return (
       <form onSubmit={this.onSubmit}>
       <fieldset>
-        <label>
-          Spot:
-          <select name="spot" value={this.state.spot} onChange={this.handleChange}>
-            <optgroup label="Rivers:">
-      {Rivers.map( (r, index) =>
-        <option key={index} value={r.slug}>{r.name}</option>
-      )}
-            </optgroup>
-            <optgroup label="Surf Breaks">
-              {Breaks.map( (r, index) =>
-                <option key={index} value={r.slug}>{r.name}</option>
-              )}
-
-            </optgroup>
-          </select>
-        </label>
+        <SelectSpot onChange={this.handleChange} current={this.state.spot}/>
         <label >
           date:
           <input name="date" type="text" value={this.state.date} onChange={this.handleChange}/>
         </label>
-
+        <label>
+          Quality:
+          <StarRating
+            numberOfStars="5"
+            currentRating={this.props.rating || 0 }
+            onClick={this.setRating}
+          />
+        </label>
       </fieldset>
         <fieldset  className="entry">
       <label for="entry">
