361b287ecee1841d9807b74104567c6a0ba2e050
Matt Price <matt.price@utoronto.ca>
2020-01-10 13:11:09 -0500

    statemaps: simplify, add selector
    get ready for a possible conversion to redux hooks.

--- src/utils/stateMaps.js
+++ src/utils/stateMaps.js
@@ -1,6 +1,15 @@
 // so simple! but keepineg here in case I change state structure later
 export const userSelector  = (state) => state.user?.user;
 
+export const journalSelector = (state) => {
+  const loggedIn = state.user?.user 
+  return {
+    entries: loggedIn ? state.journal[loggedIn] : state.journal.anonymous,
+    uid: loggedIn,
+    filters: state.journalFilter
+  };
+}
+
 export function mapJournalToProps(state) {
   const loggedIn = state.user?.user
   return {
@@ -10,10 +19,10 @@ export function mapJournalToProps(state) {
   };
 }
 
-export function mapUserToProps(state) {
-  const loggedIn = state.user?.user
+export const mapUserToProps = (state) => {
+  //const loggedIn = state.user?.user
   return {
-    uid: loggedIn
+    uid: state.user?.user
   };
 }
 
